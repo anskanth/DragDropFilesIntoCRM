2c53b6e00d343dda5f0e48742ef7fb00aa434c0e
Srikanth <ansrikanth@in.ibm.com>
2017-01-24 15:42:15 +0530

    Add project files.

---
+++ html5dragdrop/fdd_/Javascript/CrmScripts.js
@@ -0,0 +1,103 @@
+﻿/// <reference path="FileUploads.js" />
+var WEBAPI_URI_VERSION= "/api/data/v8.2/";
+
+function GetWebApiUrl()
+{
+    var clientUrl = Xrm.Page.context.getClientUrl();
+
+    return clientUrl + WEBAPI_URI_VERSION;
+}
+
+function PopulateEntityInfo()
+{
+    var query = "EntityDefinitions?$select=EntitySetName,LogicalName&$filter=ObjectTypeCode eq " + entityTypecode;
+
+    var data = RetriveDataUsingWebApi(query, undefined, false, EntityInfoRetrived);
+
+}
+
+function EntityInfoRetrived(data)
+{
+    if (data != null) {
+        entityCollectionName = data.value[0].EntitySetName;
+        logicalName = data.value[0].LogicalName;
+    }
+}
+
+function RetriveDataUsingWebApi(webApiQuery, data, isAsync, callbackFn)
+{
+    var req = new XMLHttpRequest()
+    req.open("GET", encodeURI(GetWebApiUrl() + webApiQuery), isAsync);
+    SetRequestHeaders(req);
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 /* complete */) {
+            req.onreadystatechange = null;
+            if (req.status == 200) {
+                callbackFn(JSON.parse(req.response));
+            }
+            else {
+                var error = JSON.parse(req.response).error;
+                alert(error.message);
+                return null;
+            }
+        }
+    };
+    req.send(data);
+}
+
+function UploadFileToCrm(filecontent, file, successCallback) {
+
+    var content = ArrayBufferToBase64(filecontent);
+
+    var data = {};
+    data.filename = file.name;
+    data.documentbody = content;
+    data.mimetype = file.type;
+    data.subject = title;
+    data.notetext = description;
+    data["objectid_" + logicalName + "@odata.bind"] = "/" + entityCollectionName + "(" + recordId + ")";
+
+    var query = "/annotations";
+    var req = new XMLHttpRequest()
+    req.open("POST", encodeURI(GetWebApiUrl() + query), true);
+    req.setRequestHeader("Accept", "application/json");
+    req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    req.setRequestHeader("OData-MaxVersion", "4.0");
+    req.setRequestHeader("OData-Version", "4.0");
+
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 /* complete */) {
+            req.onreadystatechange = null;
+            if (req.status == 204) {
+                //successCallback();
+                successCallback(file);
+            } else {
+                var error = JSON.parse(req.response).error;
+                alert(error.message);
+            }
+        }
+    };
+
+    req.send(JSON.stringify(data));
+
+}
+
+function FileUploadedSuccessfully(file)
+{
+    noOfFilesUploaded += 1;
+    SetMessage("msg1", "File uploaded - " + file.name);
+    if (noOfFilesUploaded == filesToUpload.length)
+    {
+        filesToUpload = [];
+        noOfFilesUploaded = 0;
+        setTimeout(function () { SetMessage("msg1", "Upload completed !!"); SetMessage("msg", ""); }, 1000);
+    }
+}
+
+function SetRequestHeaders(req)
+{
+    req.setRequestHeader("Accept", "application/json");
+    req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    req.setRequestHeader("OData-MaxVersion", "4.0");
+    req.setRequestHeader("OData-Version", "4.0");
+}
---
+++ html5dragdrop/fdd_/Javascript/FileUploads.js
@@ -0,0 +1,151 @@
+﻿var filesToUpload = [];
+var noOfFilesUploaded = 0;
+var title = "";
+var description = "";
+var recordId = "";
+var entityTypecode = "";
+var entityCollectionName = "";
+var logicalName = "";
+
+
+_Get = function (controlName) {
+    var element = document.getElementById(controlName);
+    if (element !== undefined)
+        return element;
+
+    return null;
+};
+
+_GetValue = function (controlName) {
+    var element = _Get(controlName);
+    if (element !== null) {
+        return element.value;
+    }
+
+    return null;
+};
+
+GetQueryParameterByName = function (name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+SetMessage = function (controlName, value) {
+    var element = _Get(controlName);
+    if (element !== null)
+        element.innerText = value;
+};
+
+StopFileLoading = function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+};
+
+ArrayBufferToBase64 = function (buffer) { // Convert Array Buffer to Base 64 string
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+};
+
+AttachEvents = function () {
+    var fileSel = _Get("fileCtrl");
+    var fileDiv = _Get("filedroplocation");
+    var uploadBtn = _Get("uploadBtn");
+    if (fileSel !== null) {
+        fileSel.addEventListener("change", FileSelected, false);
+    }
+    if (fileDiv !== null) {
+        fileDiv.addEventListener("dragover", FileDrag, false);
+        fileDiv.addEventListener("dragleave", FileDragLeave, false);
+        fileDiv.addEventListener("drop", FileSelected, false);
+    }
+    if (uploadBtn !== null) {
+        uploadBtn.addEventListener("click", UploadFiles, false);
+    }
+};
+
+UploadIndividualFile = function (file) {
+    var fileReader = new FileReader();
+
+    fileReader.onloadend = function (e) {
+        var content = e.target.result;
+        console.log(file.name + "--" + content.byteLength);
+        UploadFileToCrm(content, file, FileUploadedSuccessfully);
+    };
+    fileReader.readAsArrayBuffer(file);
+};
+
+
+function DocumentLoaded()
+{
+    entityTypecode = GetQueryParameterByName("type");
+    recordId = GetQueryParameterByName("id");
+    if (entityTypecode == "" || entityTypecode == undefined || recordId == "" || recordId == undefined) {
+        alert("Unable to identify the entity type code/Id !! You can't use the Drag/Drop functionality now.");
+        return;
+    }
+    recordId = recordId.replace("{","").replace("}","");
+    PopulateEntityInfo();
+    if (entityCollectionName == "" || entityCollectionName == undefined)
+    {
+        alert("Some thing went wrong, Unable to identify the entity !! You can't use the Drag/Drop functionality now.");
+        return;
+    }
+    AttachEvents();
+}
+
+function FileDragLeave(e)
+{
+    this.classList.remove("dragover");
+    StopFileLoading(e);
+}
+
+function FileDrag(e) {
+    this.classList.add("dragover");
+    StopFileLoading(e);
+}
+
+function FileSelected(e) {
+    this.classList.remove("dragover");
+    StopFileLoading(e);
+
+    var files = e.target.files || e.dataTransfer.files;
+
+    for (var i = 0; i < files.length; i++) {
+        filesToUpload.push(files[i]);
+    }
+
+    SetMessage("msg", "No of Files Selected : " + filesToUpload.length);
+}
+
+function UploadFiles()
+{
+    var filesCount=filesToUpload.length;
+    if (filesCount <= 0) {
+        alert("No Files to upload !! Please Drag & Drop files or use Browse button to select files.");
+        return;
+    }
+    
+  
+    if (_Get("chkTitle").checked)
+        title = prompt("Please provide Title for Notes");
+
+    if (_Get("chkDescription").checked)
+        description = prompt("Please provide Description for Notes");
+
+    for (var i = 0; i < filesCount; i++) {
+        UploadIndividualFile(filesToUpload[i]);
+    }
+    
+}
